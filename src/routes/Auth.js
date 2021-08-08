0ae62b2853607fcc2ebb892efd032a9e2298db42
seyxxn <lucy9480@gmail.com>
2021-08-09 08:58:01 +0900

    2.4 Log In

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,60 +1,73 @@
-import { auth } from "firebase/app";
 import React, { useState } from "react";
-import { authService } from "../fbase";
+import { authService } from "fbase";
 
-const Auth =  () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [newAccout, setNewAccout] = useState(true);
-    const onChange = (event) => {
-        const {target: {name,value}} = event;
-        if(name === "email"){
-            setEmail(value);
-        } else if (name ==="password"){
-            setPassword(value);
-        }
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
     }
-    const onSubmit = async(event) => {
-        event.preventDefault();
-        try {
-            let data;
-            if(newAccout){
-                data = await authService.createUserWithEmailAndPassword(
-                    email, password);
-            }else {
-                data = await authService.signInWithEmailAndPassword(email, password);
-            }
-            console.log(data);
-        } catch(error) {
-            console.log(error);
-        }
-    };
-    return (
-    <div>
-        <form onSubmit={onSubmit}>
-            <input
-            name = "email"
-            type="email"
-            placeholder="Email"
-            required value={email} 
-            onChange = {onChange} />
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
 
-            <input
-            name = "password"
-            type = "password"
-            placeholder="password"
-            required value = {password}
-            onChange = {onChange} />
-
-            <input
-            type = "submit"
-            value = {newAccout ? "Create Account" : "Log In"} />
-        </form>
+  return (
     <div>
-            <button> Continue with Google</button>
-            <button>Continue with Github</button>
-        </div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign In"}
+        />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
     </div>
-    );
+  );
 };
 export default Auth;
