32526072961ac40e4835079d166b7e7f4e53c304
seyxxn <lucy9480@gmail.com>
2021-08-11 17:52:21 +0900

    2.6 Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,17 @@
 import React from "react";
+import { authService } from "fbase";
+import { useHistory } from "react-router-dom";
 
-const Profile =  () => <span>Profile</span>;
-export default Profile;
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+export default () => {
+    const history = useHistory();
+    const onLogOutCliak = () => {
+        authService.signOut();
+        history.push("/");
+    };
+    return (
+        <>
+             <button onClick = {onLogOutCliak}> Log Out </button>
+        </>
+    );
+};
